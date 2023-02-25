0aeb5f2a00c02f681c27ff7b7953ad714f3f5877
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-02-25 11:14:16 -0800

    Cambio

---
+++ OTRAS COSAS PERSONALES/COSA/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CalcularTotal();
+        }
+
+        private void Boton_Click(object sender, EventArgs e)
+        {
+            // declarar
+            double pa1 = 0;
+            double pa2 = 0;
+            double pa3 = 0;
+            double pa4 = 0;
+            double pa5 = 0;
+            double subtotal = 0;
+            double iva = 0;
+            double total = 0;
+            //entrada
+            pa1=Double.Parse(textBox1.Text);
+            pa2=Double.Parse(textBox2.Text);
+            pa3=Double.Parse(textBox3.Text);
+            pa4=Double.Parse(textBox4.Text);
+            pa5=Double.Parse(textBox5.Text);
+            //proceso
+            subtotal=pa1+ pa2+ pa3+ pa4+ pa5;
+            iva = subtotal * 0.08;
+            total = subtotal + iva;
+            //salida
+            cosa.Text = subtotal.ToString();
+            IVA.Text = iva.ToString();
+            TOTAL.Text = total.ToString();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotonAparece_Click(object sender, EventArgs e)
+        {
+            CalcularTotal();
+        }
+
+        private void OtraCosa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcularTotal()
+        {
+            // declarar
+            double pa1 = 0;
+            double pa2 = 0;
+            double pa3 = 0;
+            double pa4 = 0;
+            double pa5 = 0;
+            double subtotal = 0;
+            double iva = 0;
+            double total = 0;
+            //entrada
+            pa1 = Double.Parse(textBox1.Text);
+            pa2 = Double.Parse(textBox2.Text);
+            pa3 = Double.Parse(textBox3.Text);
+            pa4 = Double.Parse(textBox4.Text);
+            pa5 = Double.Parse(textBox5.Text);
+            //proceso
+            subtotal = pa1 + pa2 + pa3 + pa4 + pa5;
+            iva = subtotal * 0.08;
+            total = subtotal + iva;
+            //salida
+            cosa.Text = subtotal.ToString();
+            IVA.Text = iva.ToString();
+            TOTAL.Text = total.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            CalcularTotal();
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            CalcularTotal();
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            CalcularTotal();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            CalcularTotal();
+        }
+    }
+}
---
+++ OTRAS COSAS PERSONALES/PracticaMedianoche/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaMedianoche
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            float PA1, PA2, PA3, PA4, PA5, SUBTOTAL, IVA, TOTAL;
+            //Entrada
+            PA1 = System.Single.Parse(txtPA1.Text);
+            PA2 = System.Single.Parse(txtPA2.Text);
+            PA3 = System.Single.Parse(txtPA3.Text);
+            PA4 = System.Single.Parse(txtPA4.Text);
+            PA5 = System.Single.Parse(txtPA5.Text);
+            //Proceso
+            SUBTOTAL = PA1 + PA2 + PA3 + PA4 + PA5;
+            IVA = (float)(SUBTOTAL * 0.08);
+            TOTAL = SUBTOTAL + IVA;
+            //Salida
+            txtSUB.Text=SUBTOTAL.ToString();
+            txtIVA.Text=IVA.ToString();
+            txtTOTAL.Text=TOTAL.ToString();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtNA1.Clear();
+            txtNA2.Clear();
+            txtNA3.Clear();
+            txtNA4.Clear();
+            txtNA5.Clear();
+            txtPA1.Clear();
+            txtPA2.Clear();
+            txtPA3.Clear();
+            txtPA4.Clear();
+            txtPA5.Clear();
+            txtSUB.Clear();
+            txtIVA.Clear();
+            txtTOTAL.Clear();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
