3964f1b78360b00283b2ab906cf183cbf8eebeda
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-02-24 21:02:30 -0800

    Practica medianoche

---
+++ UNIDAD_2/PracticaMedianoche/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace PracticaMedianoche
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LABELTITULO = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNA1 = new System.Windows.Forms.TextBox();
+            this.txtNA2 = new System.Windows.Forms.TextBox();
+            this.txtNA3 = new System.Windows.Forms.TextBox();
+            this.txtNA4 = new System.Windows.Forms.TextBox();
+            this.txtNA5 = new System.Windows.Forms.TextBox();
+            this.txtPA1 = new System.Windows.Forms.TextBox();
+            this.txtPA2 = new System.Windows.Forms.TextBox();
+            this.txtPA3 = new System.Windows.Forms.TextBox();
+            this.txtPA4 = new System.Windows.Forms.TextBox();
+            this.txtPA5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSUB = new System.Windows.Forms.TextBox();
+            this.txtIVA = new System.Windows.Forms.TextBox();
+            this.txtTOTAL = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LABELTITULO
+            // 
+            this.LABELTITULO.AutoSize = true;
+            this.LABELTITULO.Location = new System.Drawing.Point(252, 24);
+            this.LABELTITULO.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LABELTITULO.Name = "LABELTITULO";
+            this.LABELTITULO.Size = new System.Drawing.Size(47, 13);
+            this.LABELTITULO.TabIndex = 0;
+            this.LABELTITULO.Text = "TIENDA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RFC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FECHA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DIRECCION";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ARTICULO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(315, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "PRECIO";
+            // 
+            // txtNA1
+            // 
+            this.txtNA1.Location = new System.Drawing.Point(167, 161);
+            this.txtNA1.Name = "txtNA1";
+            this.txtNA1.Size = new System.Drawing.Size(100, 20);
+            this.txtNA1.TabIndex = 6;
+            // 
+            // txtNA2
+            // 
+            this.txtNA2.Location = new System.Drawing.Point(167, 187);
+            this.txtNA2.Name = "txtNA2";
+            this.txtNA2.Size = new System.Drawing.Size(100, 20);
+            this.txtNA2.TabIndex = 7;
+            // 
+            // txtNA3
+            // 
+            this.txtNA3.Location = new System.Drawing.Point(167, 213);
+            this.txtNA3.Name = "txtNA3";
+            this.txtNA3.Size = new System.Drawing.Size(100, 20);
+            this.txtNA3.TabIndex = 8;
+            // 
+            // txtNA4
+            // 
+            this.txtNA4.Location = new System.Drawing.Point(167, 239);
+            this.txtNA4.Name = "txtNA4";
+            this.txtNA4.Size = new System.Drawing.Size(100, 20);
+            this.txtNA4.TabIndex = 9;
+            // 
+            // txtNA5
+            // 
+            this.txtNA5.Location = new System.Drawing.Point(167, 265);
+            this.txtNA5.Name = "txtNA5";
+            this.txtNA5.Size = new System.Drawing.Size(100, 20);
+            this.txtNA5.TabIndex = 10;
+            // 
+            // txtPA1
+            // 
+            this.txtPA1.Location = new System.Drawing.Point(290, 161);
+            this.txtPA1.Name = "txtPA1";
+            this.txtPA1.Size = new System.Drawing.Size(100, 20);
+            this.txtPA1.TabIndex = 11;
+            // 
+            // txtPA2
+            // 
+            this.txtPA2.Location = new System.Drawing.Point(290, 187);
+            this.txtPA2.Name = "txtPA2";
+            this.txtPA2.Size = new System.Drawing.Size(100, 20);
+            this.txtPA2.TabIndex = 12;
+            // 
+            // txtPA3
+            // 
+            this.txtPA3.Location = new System.Drawing.Point(290, 213);
+            this.txtPA3.Name = "txtPA3";
+            this.txtPA3.Size = new System.Drawing.Size(100, 20);
+            this.txtPA3.TabIndex = 13;
+            // 
+            // txtPA4
+            // 
+            this.txtPA4.Location = new System.Drawing.Point(290, 239);
+            this.txtPA4.Name = "txtPA4";
+            this.txtPA4.Size = new System.Drawing.Size(100, 20);
+            this.txtPA4.TabIndex = 14;
+            // 
+            // txtPA5
+            // 
+            this.txtPA5.Location = new System.Drawing.Point(290, 265);
+            this.txtPA5.Name = "txtPA5";
+            this.txtPA5.Size = new System.Drawing.Size(100, 20);
+            this.txtPA5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "SUBTOTAL";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(238, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "IVA";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(225, 354);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "TOTAL";
+            // 
+            // txtSUB
+            // 
+            this.txtSUB.Location = new System.Drawing.Point(290, 299);
+            this.txtSUB.Name = "txtSUB";
+            this.txtSUB.Size = new System.Drawing.Size(100, 20);
+            this.txtSUB.TabIndex = 19;
+            // 
+            // txtIVA
+            // 
+            this.txtIVA.Location = new System.Drawing.Point(290, 325);
+            this.txtIVA.Name = "txtIVA";
+            this.txtIVA.Size = new System.Drawing.Size(100, 20);
+            this.txtIVA.TabIndex = 20;
+            // 
+            // txtTOTAL
+            // 
+            this.txtTOTAL.Location = new System.Drawing.Point(290, 351);
+            this.txtTOTAL.Name = "txtTOTAL";
+            this.txtTOTAL.Size = new System.Drawing.Size(100, 20);
+            this.txtTOTAL.TabIndex = 21;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(162, 402);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 26);
+            this.btncalcular.TabIndex = 22;
+            this.btncalcular.Text = "CALCULAR";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(249, 402);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(75, 26);
+            this.btnlimpiar.TabIndex = 23;
+            this.btnlimpiar.Text = "LIMPIAR";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(342, 402);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 26);
+            this.btnsalir.TabIndex = 24;
+            this.btnsalir.Text = "SALIR";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 530);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtTOTAL);
+            this.Controls.Add(this.txtIVA);
+            this.Controls.Add(this.txtSUB);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPA5);
+            this.Controls.Add(this.txtPA4);
+            this.Controls.Add(this.txtPA3);
+            this.Controls.Add(this.txtPA2);
+            this.Controls.Add(this.txtPA1);
+            this.Controls.Add(this.txtNA5);
+            this.Controls.Add(this.txtNA4);
+            this.Controls.Add(this.txtNA3);
+            this.Controls.Add(this.txtNA2);
+            this.Controls.Add(this.txtNA1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LABELTITULO);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LABELTITULO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNA1;
+        private System.Windows.Forms.TextBox txtNA2;
+        private System.Windows.Forms.TextBox txtNA3;
+        private System.Windows.Forms.TextBox txtNA4;
+        private System.Windows.Forms.TextBox txtNA5;
+        private System.Windows.Forms.TextBox txtPA1;
+        private System.Windows.Forms.TextBox txtPA2;
+        private System.Windows.Forms.TextBox txtPA3;
+        private System.Windows.Forms.TextBox txtPA4;
+        private System.Windows.Forms.TextBox txtPA5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSUB;
+        private System.Windows.Forms.TextBox txtIVA;
+        private System.Windows.Forms.TextBox txtTOTAL;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
