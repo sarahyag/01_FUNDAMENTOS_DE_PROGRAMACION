8ce568d8b4de423a5588dd8c2f88f854240daf78
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-03-01 15:17:19 -0800

    Libras a kilos

---
+++ UNIDAD_2/PracticaLibrasAKilos/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PracticaLibrasAKilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelLibras = new System.Windows.Forms.Label();
+            this.labelKilos = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtLibras = new System.Windows.Forms.TextBox();
+            this.txtKilos = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(293, 9);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(218, 24);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Convertir libras a kilos";
+            // 
+            // labelLibras
+            // 
+            this.labelLibras.AutoSize = true;
+            this.labelLibras.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLibras.Location = new System.Drawing.Point(297, 71);
+            this.labelLibras.Name = "labelLibras";
+            this.labelLibras.Size = new System.Drawing.Size(56, 18);
+            this.labelLibras.TabIndex = 1;
+            this.labelLibras.Text = "Libras:";
+            // 
+            // labelKilos
+            // 
+            this.labelKilos.AutoSize = true;
+            this.labelKilos.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKilos.Location = new System.Drawing.Point(306, 104);
+            this.labelKilos.Name = "labelKilos";
+            this.labelKilos.Size = new System.Drawing.Size(47, 18);
+            this.labelKilos.TabIndex = 2;
+            this.labelKilos.Text = "Kilos:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(251, 150);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 27);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtLibras
+            // 
+            this.txtLibras.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLibras.Location = new System.Drawing.Point(373, 68);
+            this.txtLibras.Name = "txtLibras";
+            this.txtLibras.Size = new System.Drawing.Size(100, 26);
+            this.txtLibras.TabIndex = 4;
+            // 
+            // txtKilos
+            // 
+            this.txtKilos.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtKilos.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKilos.Location = new System.Drawing.Point(373, 101);
+            this.txtKilos.Name = "txtKilos";
+            this.txtKilos.ReadOnly = true;
+            this.txtKilos.Size = new System.Drawing.Size(100, 26);
+            this.txtKilos.TabIndex = 5;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(340, 150);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(133, 27);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar valores";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(488, 150);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(72, 27);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtKilos);
+            this.Controls.Add(this.txtLibras);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.labelKilos);
+            this.Controls.Add(this.labelLibras);
+            this.Controls.Add(this.labelTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Label labelLibras;
+        private System.Windows.Forms.Label labelKilos;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtLibras;
+        private System.Windows.Forms.TextBox txtKilos;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ UNIDAD_2/PracticaLibrasAKilos/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaLibrasAKilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            double libras, kilos;
+            //Entrada
+            libras=System.Double.Parse(txtLibras.Text);
+            //Proceso
+            kilos = libras * 0.45;
+            //Salida
+            txtKilos.Text = kilos.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtKilos.Clear();
+            txtLibras.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
