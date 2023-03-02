e33c601e3d002834d679e265634c1a54e6ec8625
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-03-01 17:59:00 -0800

    Problema examen

---
+++ UNIDAD_2/Practica_Problema Examen II/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Practica_Problema_Examen_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.labela = new System.Windows.Forms.Label();
+            this.labelb = new System.Windows.Forms.Label();
+            this.labelNota = new System.Windows.Forms.Label();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtx = new System.Windows.Forms.TextBox();
+            this.labelx = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(300, 9);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(238, 24);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Cálculo de x=12ab^2-c";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(438, 69);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 29);
+            this.txta.TabIndex = 1;
+            this.txta.Text = "0";
+            this.txta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(251, 208);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(101, 31);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // labela
+            // 
+            this.labela.AutoSize = true;
+            this.labela.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labela.Location = new System.Drawing.Point(300, 73);
+            this.labela.Name = "labela";
+            this.labela.Size = new System.Drawing.Size(100, 22);
+            this.labela.TabIndex = 3;
+            this.labela.Text = "Valor de a:";
+            // 
+            // labelb
+            // 
+            this.labelb.AutoSize = true;
+            this.labelb.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelb.Location = new System.Drawing.Point(300, 116);
+            this.labelb.Name = "labelb";
+            this.labelb.Size = new System.Drawing.Size(101, 22);
+            this.labelb.TabIndex = 4;
+            this.labelb.Text = "Valor de b:";
+            // 
+            // labelNota
+            // 
+            this.labelNota.AutoSize = true;
+            this.labelNota.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNota.Location = new System.Drawing.Point(310, 33);
+            this.labelNota.Name = "labelNota";
+            this.labelNota.Size = new System.Drawing.Size(219, 16);
+            this.labelNota.TabIndex = 5;
+            this.labelNota.Text = "La letra \"c\" toma una constante de 3";
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.Location = new System.Drawing.Point(438, 112);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 29);
+            this.txtb.TabIndex = 6;
+            this.txtb.Text = "0";
+            this.txtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(358, 208);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(171, 31);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar valores";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(535, 208);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(69, 31);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtx
+            // 
+            this.txtx.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtx.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtx.Location = new System.Drawing.Point(438, 156);
+            this.txtx.Name = "txtx";
+            this.txtx.ReadOnly = true;
+            this.txtx.Size = new System.Drawing.Size(100, 29);
+            this.txtx.TabIndex = 10;
+            this.txtx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelx.Location = new System.Drawing.Point(300, 160);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(99, 22);
+            this.labelx.TabIndex = 9;
+            this.labelx.Text = "Valor de x:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtx);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.labelNota);
+            this.Controls.Add(this.labelb);
+            this.Controls.Add(this.labela);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.labelTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label labela;
+        private System.Windows.Forms.Label labelb;
+        private System.Windows.Forms.Label labelNota;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtx;
+        private System.Windows.Forms.Label labelx;
+    }
+}
+
---
+++ UNIDAD_2/Practica_Problema Examen II/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Problema_Examen_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            int a, b, x;
+            int c = 3;
+
+            //Entrada
+            a= System.Int16.Parse(txta.Text);
+            b= System.Int16.Parse(txtb.Text);
+
+            //Proceso
+            x = (12 * (a * (b * b)) - c);
+
+            //Salida
+            txtx.Text=x.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txta.Clear();
+            txtb.Clear();
+            txtx.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
