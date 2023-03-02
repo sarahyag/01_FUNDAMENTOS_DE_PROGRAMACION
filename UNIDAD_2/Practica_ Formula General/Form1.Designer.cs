d9f25d5021dd14e7b8de9febde67838174635102
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-03-01 17:44:06 -0800

    Cambios

---
+++ UNIDAD_2/Practica_ Formula General/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtx2 = new System.Windows.Forms.TextBox();
+            this.labelx2 = new System.Windows.Forms.Label();
+            this.txtx1 = new System.Windows.Forms.TextBox();
+            this.labelx1 = new System.Windows.Forms.Label();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.labelc = new System.Windows.Forms.Label();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.labelb = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.labela = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtx2
+            // 
+            this.txtx2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtx2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtx2.Location = new System.Drawing.Point(531, 186);
+            this.txtx2.Name = "txtx2";
+            this.txtx2.ReadOnly = true;
+            this.txtx2.Size = new System.Drawing.Size(100, 29);
+            this.txtx2.TabIndex = 27;
+            this.txtx2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelx2
+            // 
+            this.labelx2.AutoSize = true;
+            this.labelx2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelx2.Location = new System.Drawing.Point(415, 189);
+            this.labelx2.Name = "labelx2";
+            this.labelx2.Size = new System.Drawing.Size(110, 22);
+            this.labelx2.TabIndex = 26;
+            this.labelx2.Text = "Valor de x2:";
+            // 
+            // txtx1
+            // 
+            this.txtx1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtx1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtx1.Location = new System.Drawing.Point(273, 186);
+            this.txtx1.Name = "txtx1";
+            this.txtx1.ReadOnly = true;
+            this.txtx1.Size = new System.Drawing.Size(100, 29);
+            this.txtx1.TabIndex = 25;
+            this.txtx1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelx1
+            // 
+            this.labelx1.AutoSize = true;
+            this.labelx1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelx1.Location = new System.Drawing.Point(157, 189);
+            this.labelx1.Name = "labelx1";
+            this.labelx1.Size = new System.Drawing.Size(110, 22);
+            this.labelx1.TabIndex = 24;
+            this.labelx1.Text = "Valor de x1:";
+            // 
+            // txtc
+            // 
+            this.txtc.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtc.Location = new System.Drawing.Point(400, 137);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 29);
+            this.txtc.TabIndex = 23;
+            this.txtc.Text = "0";
+            this.txtc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelc
+            // 
+            this.labelc.AutoSize = true;
+            this.labelc.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelc.Location = new System.Drawing.Point(273, 140);
+            this.labelc.Name = "labelc";
+            this.labelc.Size = new System.Drawing.Size(100, 22);
+            this.labelc.TabIndex = 22;
+            this.labelc.Text = "Valor de c:";
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.Location = new System.Drawing.Point(400, 102);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 29);
+            this.txtb.TabIndex = 21;
+            this.txtb.Text = "0";
+            this.txtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelb
+            // 
+            this.labelb.AutoSize = true;
+            this.labelb.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelb.Location = new System.Drawing.Point(273, 105);
+            this.labelb.Name = "labelb";
+            this.labelb.Size = new System.Drawing.Size(101, 22);
+            this.labelb.TabIndex = 20;
+            this.labelb.Text = "Valor de b:";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(400, 67);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 29);
+            this.txta.TabIndex = 19;
+            this.txta.Text = "0";
+            this.txta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labela
+            // 
+            this.labela.AutoSize = true;
+            this.labela.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labela.Location = new System.Drawing.Point(273, 70);
+            this.labela.Name = "labela";
+            this.labela.Size = new System.Drawing.Size(100, 22);
+            this.labela.TabIndex = 18;
+            this.labela.Text = "Valor de a:";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(511, 249);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(98, 35);
+            this.btnSalir.TabIndex = 17;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(334, 249);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(164, 35);
+            this.btnLimpiar.TabIndex = 16;
+            this.btnLimpiar.Text = "Limpiar valores";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(257, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 24);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Cálculo de la fórmula general";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(216, 249);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(98, 35);
+            this.btnCalcular.TabIndex = 14;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtx2);
+            this.Controls.Add(this.labelx2);
+            this.Controls.Add(this.txtx1);
+            this.Controls.Add(this.labelx1);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.labelc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.labelb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.labela);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtx2;
+        private System.Windows.Forms.Label labelx2;
+        private System.Windows.Forms.TextBox txtx1;
+        private System.Windows.Forms.Label labelx1;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Label labelc;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Label labelb;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Label labela;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ UNIDAD_2/Practica_ Formula General/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            double a, b, c, x1, x2;
+
+            //Entrada
+            a = System.Double.Parse(txta.Text);
+            b = System.Double.Parse(txtb.Text);
+            c = System.Double.Parse(txtc.Text);
+
+            //Proceso
+            x1 = (-(b) + (Math.Sqrt((b * b) - (4 * a * c)))) / 2 * (a);
+            x2 = (-(b) - (Math.Sqrt((b * b) - (4 * a * c)))) / 2 * (a);
+
+            //Salida
+            txtx1.Text = x1.ToString();
+            txtx2.Text = x2.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txta.Clear();
+            txtb.Clear();
+            txtc.Clear();
+            txtx1.Clear();
+            txtx2.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
