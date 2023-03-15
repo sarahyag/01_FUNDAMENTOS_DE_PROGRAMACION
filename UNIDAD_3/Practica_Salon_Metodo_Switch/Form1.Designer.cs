bd20b6c92432986179a2ee53769f63623181e454
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-03-15 09:52:39 -0700

    Dias de la semana switch

---
+++ UNIDAD_3/Practica_Salon_Metodo_Switch/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Practica_Salon_Metodo_Switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labeltitulo = new System.Windows.Forms.Label();
+            this.labelnumero = new System.Windows.Forms.Label();
+            this.txtdia = new System.Windows.Forms.TextBox();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.cmdDesplegar = new System.Windows.Forms.Button();
+            this.cmdLimpiar = new System.Windows.Forms.Button();
+            this.cmdSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labeltitulo
+            // 
+            this.labeltitulo.AutoSize = true;
+            this.labeltitulo.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltitulo.Location = new System.Drawing.Point(120, 9);
+            this.labeltitulo.Name = "labeltitulo";
+            this.labeltitulo.Size = new System.Drawing.Size(220, 29);
+            this.labeltitulo.TabIndex = 0;
+            this.labeltitulo.Text = "Días de la semana";
+            // 
+            // labelnumero
+            // 
+            this.labelnumero.AutoSize = true;
+            this.labelnumero.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelnumero.Location = new System.Drawing.Point(132, 55);
+            this.labelnumero.Name = "labelnumero";
+            this.labelnumero.Size = new System.Drawing.Size(82, 22);
+            this.labelnumero.TabIndex = 1;
+            this.labelnumero.Text = "Número:";
+            // 
+            // txtdia
+            // 
+            this.txtdia.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdia.Location = new System.Drawing.Point(220, 52);
+            this.txtdia.Name = "txtdia";
+            this.txtdia.Size = new System.Drawing.Size(100, 29);
+            this.txtdia.TabIndex = 2;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMensaje.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.Location = new System.Drawing.Point(205, 91);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 22);
+            this.lblMensaje.TabIndex = 3;
+            // 
+            // cmdDesplegar
+            // 
+            this.cmdDesplegar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdDesplegar.Location = new System.Drawing.Point(34, 141);
+            this.cmdDesplegar.Name = "cmdDesplegar";
+            this.cmdDesplegar.Size = new System.Drawing.Size(119, 32);
+            this.cmdDesplegar.TabIndex = 4;
+            this.cmdDesplegar.Text = "Desplegar";
+            this.cmdDesplegar.UseVisualStyleBackColor = true;
+            this.cmdDesplegar.Click += new System.EventHandler(this.cmdDesplegar_Click);
+            // 
+            // cmdLimpiar
+            // 
+            this.cmdLimpiar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdLimpiar.Location = new System.Drawing.Point(159, 141);
+            this.cmdLimpiar.Name = "cmdLimpiar";
+            this.cmdLimpiar.Size = new System.Drawing.Size(162, 32);
+            this.cmdLimpiar.TabIndex = 5;
+            this.cmdLimpiar.Text = "Limpiar valores";
+            this.cmdLimpiar.UseVisualStyleBackColor = true;
+            this.cmdLimpiar.Click += new System.EventHandler(this.cmdLimpiar_Click);
+            // 
+            // cmdSalir
+            // 
+            this.cmdSalir.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSalir.Location = new System.Drawing.Point(327, 141);
+            this.cmdSalir.Name = "cmdSalir";
+            this.cmdSalir.Size = new System.Drawing.Size(119, 32);
+            this.cmdSalir.TabIndex = 6;
+            this.cmdSalir.Text = "Salir";
+            this.cmdSalir.UseVisualStyleBackColor = true;
+            this.cmdSalir.Click += new System.EventHandler(this.cmdSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 204);
+            this.Controls.Add(this.cmdSalir);
+            this.Controls.Add(this.cmdLimpiar);
+            this.Controls.Add(this.cmdDesplegar);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.txtdia);
+            this.Controls.Add(this.labelnumero);
+            this.Controls.Add(this.labeltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labeltitulo;
+        private System.Windows.Forms.Label labelnumero;
+        private System.Windows.Forms.TextBox txtdia;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button cmdDesplegar;
+        private System.Windows.Forms.Button cmdLimpiar;
+        private System.Windows.Forms.Button cmdSalir;
+    }
+}
+
---
+++ UNIDAD_3/Practica_Salon_Metodo_Switch/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Salon_Metodo_Switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdDesplegar_Click(object sender, EventArgs e)
+        {
+            //declaracion de variables
+            int dia;
+            string mensaje = "";
+
+            //entrada de datos
+            dia = System.Int16.Parse(txtdia.Text);
+            mensaje = lblMensaje.Text;
+
+            //metodo switch
+            switch (dia)
+            {
+                case 1:
+                    mensaje = " Lunes ";
+                    break;
+                case 2:
+                    mensaje = " Martes ";
+                    break;
+                case 3:
+                    mensaje = " Miércoles ";
+                    break;
+                case 4:
+                    mensaje = " Jueves ";
+                    break;
+                case 5:
+                    mensaje = " Viernes ";
+                    break;
+                case 6:
+                    mensaje = " Sábado ";
+                    break;
+                case 7:
+                    mensaje = " Domingo ";
+                    break;
+                default:
+                    mensaje = " Día erróneo ";
+                    break;
+            }
+            lblMensaje.Text = mensaje;
+        }
+
+        private void cmdLimpiar_Click(object sender, EventArgs e)
+        {
+            txtdia.Clear();
+            lblMensaje.Text = "";
+        }
+
+        private void cmdSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
