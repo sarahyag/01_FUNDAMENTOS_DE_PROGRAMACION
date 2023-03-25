3c262d08dc72f5eea20c9fe239fa209fe7122c4c
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-03-24 20:29:44 -0700

    Tabla de multiplicar

---
+++ UNIDAD_4/Tabla_de_multiplicar/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Tabla_de_multiplicar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.label_n = new System.Windows.Forms.Label();
+            this.txt_n = new System.Windows.Forms.TextBox();
+            this.txt_tabla = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.Location = new System.Drawing.Point(283, 9);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(279, 29);
+            this.label_titulo.TabIndex = 0;
+            this.label_titulo.Text = "Tabla de multiplicación\r\n";
+            // 
+            // label_n
+            // 
+            this.label_n.AutoSize = true;
+            this.label_n.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_n.Location = new System.Drawing.Point(254, 57);
+            this.label_n.Name = "label_n";
+            this.label_n.Size = new System.Drawing.Size(170, 22);
+            this.label_n.TabIndex = 1;
+            this.label_n.Text = "Número a ingresar:";
+            // 
+            // txt_n
+            // 
+            this.txt_n.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_n.Location = new System.Drawing.Point(430, 54);
+            this.txt_n.Name = "txt_n";
+            this.txt_n.Size = new System.Drawing.Size(132, 29);
+            this.txt_n.TabIndex = 2;
+            // 
+            // txt_tabla
+            // 
+            this.txt_tabla.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txt_tabla.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tabla.Location = new System.Drawing.Point(331, 143);
+            this.txt_tabla.Multiline = true;
+            this.txt_tabla.Name = "txt_tabla";
+            this.txt_tabla.ReadOnly = true;
+            this.txt_tabla.Size = new System.Drawing.Size(163, 274);
+            this.txt_tabla.TabIndex = 3;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(331, 102);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(89, 35);
+            this.btn_calcular.TabIndex = 4;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(426, 102);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(80, 35);
+            this.btn_limpiar.TabIndex = 5;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(371, 423);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(89, 35);
+            this.btn_salir.TabIndex = 6;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_tabla);
+            this.Controls.Add(this.txt_n);
+            this.Controls.Add(this.label_n);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_titulo;
+        private System.Windows.Forms.Label label_n;
+        private System.Windows.Forms.TextBox txt_n;
+        private System.Windows.Forms.TextBox txt_tabla;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
---
+++ UNIDAD_4/Tabla_de_multiplicar/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabla_de_multiplicar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            //declaracion de variables 
+            double x, n, res;
+
+            //entrada
+            x=System.Double.Parse(txt_n.Text);
+
+            //ciclo y desplegar
+            for (n= 1; n<=10; n=n+1) 
+            {
+                res = x * n;
+                txt_tabla.Text += x.ToString() + " * " + n.ToString() + " = " + res.ToString() + " \r\n ";
+            }
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            foreach (var txt in Controls)
+                if (txt is TextBox) 
+                    ((TextBox)txt).Clear();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
