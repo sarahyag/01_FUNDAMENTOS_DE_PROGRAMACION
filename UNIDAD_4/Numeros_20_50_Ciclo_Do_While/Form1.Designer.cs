f8effafc934965155ea38d973eac2ca55dfa88a7
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-03-30 23:26:47 -0700

    nuevos

---
+++ UNIDAD_4/Numeros_20_50_Ciclo_Do_While/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Numeros_20_50_Ciclo_Do_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_despliegue = new System.Windows.Forms.TextBox();
+            this.label_20_50 = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.txt_mayor = new System.Windows.Forms.TextBox();
+            this.txt_suma_impares = new System.Windows.Forms.TextBox();
+            this.txt_suma_pares = new System.Windows.Forms.TextBox();
+            this.label_sumatoria_impares = new System.Windows.Forms.Label();
+            this.txt_impares = new System.Windows.Forms.TextBox();
+            this.txt_pares = new System.Windows.Forms.TextBox();
+            this.label_sumatoria_pares = new System.Windows.Forms.Label();
+            this.label_impares = new System.Windows.Forms.Label();
+            this.label_pares = new System.Windows.Forms.Label();
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_despliegue
+            // 
+            this.txt_despliegue.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_despliegue.Location = new System.Drawing.Point(125, 109);
+            this.txt_despliegue.Multiline = true;
+            this.txt_despliegue.Name = "txt_despliegue";
+            this.txt_despliegue.Size = new System.Drawing.Size(136, 316);
+            this.txt_despliegue.TabIndex = 29;
+            // 
+            // label_20_50
+            // 
+            this.label_20_50.AutoSize = true;
+            this.label_20_50.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_20_50.Location = new System.Drawing.Point(100, 70);
+            this.label_20_50.Name = "label_20_50";
+            this.label_20_50.Size = new System.Drawing.Size(191, 22);
+            this.label_20_50.TabIndex = 28;
+            this.label_20_50.Text = "Números del 20 al 50";
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(489, 557);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(117, 34);
+            this.btn_salir.TabIndex = 27;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(317, 557);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(166, 34);
+            this.btn_limpiar.TabIndex = 26;
+            this.btn_limpiar.Text = "Limpiar valores";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(194, 557);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(117, 34);
+            this.btn_calcular.TabIndex = 25;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // txt_mayor
+            // 
+            this.txt_mayor.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mayor.Location = new System.Drawing.Point(259, 505);
+            this.txt_mayor.Multiline = true;
+            this.txt_mayor.Name = "txt_mayor";
+            this.txt_mayor.Size = new System.Drawing.Size(282, 46);
+            this.txt_mayor.TabIndex = 24;
+            // 
+            // txt_suma_impares
+            // 
+            this.txt_suma_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suma_impares.Location = new System.Drawing.Point(544, 462);
+            this.txt_suma_impares.Multiline = true;
+            this.txt_suma_impares.Name = "txt_suma_impares";
+            this.txt_suma_impares.Size = new System.Drawing.Size(136, 27);
+            this.txt_suma_impares.TabIndex = 23;
+            // 
+            // txt_suma_pares
+            // 
+            this.txt_suma_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suma_pares.Location = new System.Drawing.Point(347, 462);
+            this.txt_suma_pares.Multiline = true;
+            this.txt_suma_pares.Name = "txt_suma_pares";
+            this.txt_suma_pares.Size = new System.Drawing.Size(136, 27);
+            this.txt_suma_pares.TabIndex = 22;
+            // 
+            // label_sumatoria_impares
+            // 
+            this.label_sumatoria_impares.AutoSize = true;
+            this.label_sumatoria_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_sumatoria_impares.Location = new System.Drawing.Point(568, 428);
+            this.label_sumatoria_impares.Name = "label_sumatoria_impares";
+            this.label_sumatoria_impares.Size = new System.Drawing.Size(95, 22);
+            this.label_sumatoria_impares.TabIndex = 21;
+            this.label_sumatoria_impares.Text = "Sumatoria";
+            // 
+            // txt_impares
+            // 
+            this.txt_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_impares.Location = new System.Drawing.Point(544, 109);
+            this.txt_impares.Multiline = true;
+            this.txt_impares.Name = "txt_impares";
+            this.txt_impares.Size = new System.Drawing.Size(136, 316);
+            this.txt_impares.TabIndex = 20;
+            // 
+            // txt_pares
+            // 
+            this.txt_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pares.Location = new System.Drawing.Point(347, 109);
+            this.txt_pares.Multiline = true;
+            this.txt_pares.Name = "txt_pares";
+            this.txt_pares.Size = new System.Drawing.Size(136, 316);
+            this.txt_pares.TabIndex = 19;
+            // 
+            // label_sumatoria_pares
+            // 
+            this.label_sumatoria_pares.AutoSize = true;
+            this.label_sumatoria_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_sumatoria_pares.Location = new System.Drawing.Point(367, 428);
+            this.label_sumatoria_pares.Name = "label_sumatoria_pares";
+            this.label_sumatoria_pares.Size = new System.Drawing.Size(95, 22);
+            this.label_sumatoria_pares.TabIndex = 18;
+            this.label_sumatoria_pares.Text = "Sumatoria";
+            // 
+            // label_impares
+            // 
+            this.label_impares.AutoSize = true;
+            this.label_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_impares.Location = new System.Drawing.Point(540, 70);
+            this.label_impares.Name = "label_impares";
+            this.label_impares.Size = new System.Drawing.Size(160, 22);
+            this.label_impares.TabIndex = 17;
+            this.label_impares.Text = "Números impares";
+            // 
+            // label_pares
+            // 
+            this.label_pares.AutoSize = true;
+            this.label_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_pares.Location = new System.Drawing.Point(343, 70);
+            this.label_pares.Name = "label_pares";
+            this.label_pares.Size = new System.Drawing.Size(140, 22);
+            this.label_pares.TabIndex = 16;
+            this.label_pares.Text = "Números pares";
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.Location = new System.Drawing.Point(290, 26);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(251, 29);
+            this.label_titulo.TabIndex = 15;
+            this.label_titulo.Text = "Números del 20 al 50";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 634);
+            this.Controls.Add(this.txt_despliegue);
+            this.Controls.Add(this.label_20_50);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_mayor);
+            this.Controls.Add(this.txt_suma_impares);
+            this.Controls.Add(this.txt_suma_pares);
+            this.Controls.Add(this.label_sumatoria_impares);
+            this.Controls.Add(this.txt_impares);
+            this.Controls.Add(this.txt_pares);
+            this.Controls.Add(this.label_sumatoria_pares);
+            this.Controls.Add(this.label_impares);
+            this.Controls.Add(this.label_pares);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_despliegue;
+        private System.Windows.Forms.Label label_20_50;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.TextBox txt_mayor;
+        private System.Windows.Forms.TextBox txt_suma_impares;
+        private System.Windows.Forms.TextBox txt_suma_pares;
+        private System.Windows.Forms.Label label_sumatoria_impares;
+        private System.Windows.Forms.TextBox txt_impares;
+        private System.Windows.Forms.TextBox txt_pares;
+        private System.Windows.Forms.Label label_sumatoria_pares;
+        private System.Windows.Forms.Label label_impares;
+        private System.Windows.Forms.Label label_pares;
+        private System.Windows.Forms.Label label_titulo;
+    }
+}
+
