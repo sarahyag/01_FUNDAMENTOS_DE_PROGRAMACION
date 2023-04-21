c6b12b9820d6b482774687a0bd88b88302f093e2
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-04-21 01:18:08 -0700

    while 20 al 50

---
+++ Unidad_4/Numeros_20_50_Ciclo_While/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Numeros_20_50_Ciclo_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_desplegar = new System.Windows.Forms.Button();
+            this.txt_mayor = new System.Windows.Forms.TextBox();
+            this.txt_suma_impares = new System.Windows.Forms.TextBox();
+            this.txt_suma_pares = new System.Windows.Forms.TextBox();
+            this.label_suma_impares = new System.Windows.Forms.Label();
+            this.label_suma_pares = new System.Windows.Forms.Label();
+            this.txt_impares = new System.Windows.Forms.TextBox();
+            this.label_impares = new System.Windows.Forms.Label();
+            this.txt_pares = new System.Windows.Forms.TextBox();
+            this.label_pares = new System.Windows.Forms.Label();
+            this.txt_despliegue = new System.Windows.Forms.TextBox();
+            this.label_despliegue = new System.Windows.Forms.Label();
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(337, 533);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(163, 34);
+            this.btn_salir.TabIndex = 44;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(337, 493);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(163, 34);
+            this.btn_limpiar.TabIndex = 43;
+            this.btn_limpiar.Text = "Limpiar valores";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_desplegar
+            // 
+            this.btn_desplegar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_desplegar.Location = new System.Drawing.Point(337, 453);
+            this.btn_desplegar.Name = "btn_desplegar";
+            this.btn_desplegar.Size = new System.Drawing.Size(163, 34);
+            this.btn_desplegar.TabIndex = 42;
+            this.btn_desplegar.Text = "Desplegar";
+            this.btn_desplegar.UseVisualStyleBackColor = true;
+            this.btn_desplegar.Click += new System.EventHandler(this.btn_desplegar_Click);
+            // 
+            // txt_mayor
+            // 
+            this.txt_mayor.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mayor.Location = new System.Drawing.Point(301, 404);
+            this.txt_mayor.Name = "txt_mayor";
+            this.txt_mayor.Size = new System.Drawing.Size(467, 29);
+            this.txt_mayor.TabIndex = 41;
+            // 
+            // txt_suma_impares
+            // 
+            this.txt_suma_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suma_impares.Location = new System.Drawing.Point(668, 369);
+            this.txt_suma_impares.Name = "txt_suma_impares";
+            this.txt_suma_impares.Size = new System.Drawing.Size(100, 29);
+            this.txt_suma_impares.TabIndex = 40;
+            // 
+            // txt_suma_pares
+            // 
+            this.txt_suma_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suma_pares.Location = new System.Drawing.Point(420, 369);
+            this.txt_suma_pares.Name = "txt_suma_pares";
+            this.txt_suma_pares.Size = new System.Drawing.Size(100, 29);
+            this.txt_suma_pares.TabIndex = 39;
+            // 
+            // label_suma_impares
+            // 
+            this.label_suma_impares.AutoSize = true;
+            this.label_suma_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_suma_impares.Location = new System.Drawing.Point(603, 372);
+            this.label_suma_impares.Name = "label_suma_impares";
+            this.label_suma_impares.Size = new System.Drawing.Size(59, 22);
+            this.label_suma_impares.TabIndex = 38;
+            this.label_suma_impares.Text = "Suma";
+            // 
+            // label_suma_pares
+            // 
+            this.label_suma_pares.AutoSize = true;
+            this.label_suma_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_suma_pares.Location = new System.Drawing.Point(355, 372);
+            this.label_suma_pares.Name = "label_suma_pares";
+            this.label_suma_pares.Size = new System.Drawing.Size(59, 22);
+            this.label_suma_pares.TabIndex = 37;
+            this.label_suma_pares.Text = "Suma";
+            // 
+            // txt_impares
+            // 
+            this.txt_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_impares.Location = new System.Drawing.Point(549, 77);
+            this.txt_impares.Multiline = true;
+            this.txt_impares.Name = "txt_impares";
+            this.txt_impares.Size = new System.Drawing.Size(219, 286);
+            this.txt_impares.TabIndex = 36;
+            // 
+            // label_impares
+            // 
+            this.label_impares.AutoSize = true;
+            this.label_impares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_impares.Location = new System.Drawing.Point(581, 52);
+            this.label_impares.Name = "label_impares";
+            this.label_impares.Size = new System.Drawing.Size(174, 22);
+            this.label_impares.TabIndex = 35;
+            this.label_impares.Text = "Números impares";
+            // 
+            // txt_pares
+            // 
+            this.txt_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pares.Location = new System.Drawing.Point(301, 77);
+            this.txt_pares.Multiline = true;
+            this.txt_pares.Name = "txt_pares";
+            this.txt_pares.Size = new System.Drawing.Size(219, 286);
+            this.txt_pares.TabIndex = 34;
+            // 
+            // label_pares
+            // 
+            this.label_pares.AutoSize = true;
+            this.label_pares.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_pares.Location = new System.Drawing.Point(333, 52);
+            this.label_pares.Name = "label_pares";
+            this.label_pares.Size = new System.Drawing.Size(152, 22);
+            this.label_pares.TabIndex = 33;
+            this.label_pares.Text = "Números pares";
+            // 
+            // txt_despliegue
+            // 
+            this.txt_despliegue.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_despliegue.Location = new System.Drawing.Point(45, 77);
+            this.txt_despliegue.Multiline = true;
+            this.txt_despliegue.Name = "txt_despliegue";
+            this.txt_despliegue.Size = new System.Drawing.Size(219, 286);
+            this.txt_despliegue.TabIndex = 32;
+            // 
+            // label_despliegue
+            // 
+            this.label_despliegue.AutoSize = true;
+            this.label_despliegue.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_despliegue.Location = new System.Drawing.Point(45, 52);
+            this.label_despliegue.Name = "label_despliegue";
+            this.label_despliegue.Size = new System.Drawing.Size(219, 22);
+            this.label_despliegue.TabIndex = 31;
+            this.label_despliegue.Text = "Números desplegados";
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.Location = new System.Drawing.Point(283, 9);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(251, 29);
+            this.label_titulo.TabIndex = 30;
+            this.label_titulo.Text = "Números del 20 al 50";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 591);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_desplegar);
+            this.Controls.Add(this.txt_mayor);
+            this.Controls.Add(this.txt_suma_impares);
+            this.Controls.Add(this.txt_suma_pares);
+            this.Controls.Add(this.label_suma_impares);
+            this.Controls.Add(this.label_suma_pares);
+            this.Controls.Add(this.txt_impares);
+            this.Controls.Add(this.label_impares);
+            this.Controls.Add(this.txt_pares);
+            this.Controls.Add(this.label_pares);
+            this.Controls.Add(this.txt_despliegue);
+            this.Controls.Add(this.label_despliegue);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_desplegar;
+        private System.Windows.Forms.TextBox txt_mayor;
+        private System.Windows.Forms.TextBox txt_suma_impares;
+        private System.Windows.Forms.TextBox txt_suma_pares;
+        private System.Windows.Forms.Label label_suma_impares;
+        private System.Windows.Forms.Label label_suma_pares;
+        private System.Windows.Forms.TextBox txt_impares;
+        private System.Windows.Forms.Label label_impares;
+        private System.Windows.Forms.TextBox txt_pares;
+        private System.Windows.Forms.Label label_pares;
+        private System.Windows.Forms.TextBox txt_despliegue;
+        private System.Windows.Forms.Label label_despliegue;
+        private System.Windows.Forms.Label label_titulo;
+    }
+}
+
---
+++ Unidad_4/Numeros_20_50_Ciclo_While/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_20_50_Ciclo_While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_desplegar_Click(object sender, EventArgs e)
+        {
+            //declaracion de variables
+            int n, s1=0, s2=0;
+
+            //ciclo while en el despliegue
+            n = 20;
+            while (n <= 50)
+            {
+                //ciclo del despliegue de numeros
+                {
+                    txt_despliegue.Text += n.ToString() + "\r\n";
+                }
+
+                //ciclo de los numeros pares e impares
+                {
+                    if (n % 2 == 0)
+                    {
+                        txt_pares.Text += n.ToString() + "\r\n";
+                        s1 = s1 + n;
+                        txt_suma_pares.Text = s1.ToString();
+                    }
+                    else if (n % 2 == 1)
+                    {
+                        txt_impares.Text += n.ToString() + "\r\n";
+                        s2 = s2 + n;
+                        txt_suma_impares.Text = s2.ToString();
+                    }
+                    if (s1 > s2)
+                    {
+                        txt_mayor.Text = " La suma de pares es la mayor con " + s1.ToString();
+                    }
+                    else
+                        txt_mayor.Text = " La suma de impares es la mayor con " + s2.ToString();
+                }
+                n++;
+            }
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            foreach (var txt in Controls)
+            {
+                if (txt is TextBox)
+                    ((TextBox)txt).Clear();
+            }
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
