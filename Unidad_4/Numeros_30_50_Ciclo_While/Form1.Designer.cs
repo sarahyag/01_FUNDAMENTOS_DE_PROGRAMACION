5ba1388bbe9c39f5f54d4a0dad31bd3b562eceab
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-04-21 01:11:13 -0700

    while 30 al 50

---
+++ Unidad_4/Numeros_30_50_Ciclo_While/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Numeros_30_50_Ciclo_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_desplegar = new System.Windows.Forms.Button();
+            this.txt_suma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_multiplos_3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_descendente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ascendente = new System.Windows.Forms.TextBox();
+            this.label_ascendente = new System.Windows.Forms.Label();
+            this.label_despliegue = new System.Windows.Forms.Label();
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(500, 531);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(123, 40);
+            this.btn_salir.TabIndex = 38;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(319, 531);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(175, 40);
+            this.btn_limpiar.TabIndex = 37;
+            this.btn_limpiar.Text = "Limpiar valores";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_desplegar
+            // 
+            this.btn_desplegar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_desplegar.Location = new System.Drawing.Point(190, 531);
+            this.btn_desplegar.Name = "btn_desplegar";
+            this.btn_desplegar.Size = new System.Drawing.Size(123, 40);
+            this.btn_desplegar.TabIndex = 36;
+            this.btn_desplegar.Text = "Desplegar";
+            this.btn_desplegar.UseVisualStyleBackColor = true;
+            this.btn_desplegar.Click += new System.EventHandler(this.btn_desplegar_Click);
+            // 
+            // txt_suma
+            // 
+            this.txt_suma.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suma.Location = new System.Drawing.Point(489, 482);
+            this.txt_suma.Name = "txt_suma";
+            this.txt_suma.Size = new System.Drawing.Size(100, 29);
+            this.txt_suma.TabIndex = 35;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(239, 485);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 22);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Suma de los múltiplos de 3:";
+            // 
+            // txt_multiplos_3
+            // 
+            this.txt_multiplos_3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_multiplos_3.Location = new System.Drawing.Point(489, 123);
+            this.txt_multiplos_3.Multiline = true;
+            this.txt_multiplos_3.Name = "txt_multiplos_3";
+            this.txt_multiplos_3.Size = new System.Drawing.Size(155, 342);
+            this.txt_multiplos_3.TabIndex = 33;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(496, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 22);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Múltiplos de 3";
+            // 
+            // txt_descendente
+            // 
+            this.txt_descendente.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_descendente.Location = new System.Drawing.Point(328, 123);
+            this.txt_descendente.Multiline = true;
+            this.txt_descendente.Name = "txt_descendente";
+            this.txt_descendente.Size = new System.Drawing.Size(155, 342);
+            this.txt_descendente.TabIndex = 31;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(339, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 22);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Descendente";
+            // 
+            // txt_ascendente
+            // 
+            this.txt_ascendente.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ascendente.Location = new System.Drawing.Point(167, 123);
+            this.txt_ascendente.Multiline = true;
+            this.txt_ascendente.Name = "txt_ascendente";
+            this.txt_ascendente.Size = new System.Drawing.Size(155, 342);
+            this.txt_ascendente.TabIndex = 29;
+            // 
+            // label_ascendente
+            // 
+            this.label_ascendente.AutoSize = true;
+            this.label_ascendente.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ascendente.Location = new System.Drawing.Point(187, 98);
+            this.label_ascendente.Name = "label_ascendente";
+            this.label_ascendente.Size = new System.Drawing.Size(120, 22);
+            this.label_ascendente.TabIndex = 28;
+            this.label_ascendente.Text = "Ascendente";
+            // 
+            // label_despliegue
+            // 
+            this.label_despliegue.AutoSize = true;
+            this.label_despliegue.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_despliegue.Location = new System.Drawing.Point(293, 52);
+            this.label_despliegue.Name = "label_despliegue";
+            this.label_despliegue.Size = new System.Drawing.Size(226, 22);
+            this.label_despliegue.TabIndex = 27;
+            this.label_despliegue.Text = "Desplegados de forma:";
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.Location = new System.Drawing.Point(279, 9);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(251, 29);
+            this.label_titulo.TabIndex = 26;
+            this.label_titulo.Text = "Números del 30 al 50";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 653);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_desplegar);
+            this.Controls.Add(this.txt_suma);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_multiplos_3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_descendente);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ascendente);
+            this.Controls.Add(this.label_ascendente);
+            this.Controls.Add(this.label_despliegue);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_desplegar;
+        private System.Windows.Forms.TextBox txt_suma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_multiplos_3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_descendente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ascendente;
+        private System.Windows.Forms.Label label_ascendente;
+        private System.Windows.Forms.Label label_despliegue;
+        private System.Windows.Forms.Label label_titulo;
+    }
+}
+
---
+++ Unidad_4/Numeros_30_50_Ciclo_While/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_30_50_Ciclo_While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_desplegar_Click(object sender, EventArgs e)
+        {
+            //declaracion de variables
+            int n, s = 0;
+
+            //ciclo while del despliegue de numeros de forma aescendente
+            n = 30;
+            while (n <= 50)
+            {
+                txt_ascendente.Text += n.ToString() + "\r\n";
+                n++;
+            }
+
+            //ciclo while del despliegue de numeros de forma descendente
+            n = 50;
+            while (n >= 30)
+            {
+                txt_descendente.Text += n.ToString() + "\r\n";
+                n--;
+            }
+
+            //ciclo while del despliegue de multiplos de 3
+            n = 30;
+            while (n <= 50)
+            {
+                txt_multiplos_3.Text += n.ToString() + "\r\n";
+                s = s + n;
+                txt_suma.Text = s.ToString();
+                n += 3;
+            }
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            foreach (var txt in Controls)
+            {
+                if (txt is TextBox)
+                    ((TextBox)txt).Clear();
+            }
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
