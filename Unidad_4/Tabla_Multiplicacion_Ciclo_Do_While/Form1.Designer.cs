eab33e2aa09d0418f03578ebc17fa11980dd018d
sarahyag <126308187+sarahyag@users.noreply.github.com>
2023-04-21 00:17:55 -0700

    a

---
+++ Unidad_4/Tabla_Multiplicacion_Ciclo_Do_While/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Tabla_Multiplicacion_Ciclo_Do_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.txt_tabla = new System.Windows.Forms.TextBox();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_desplegar = new System.Windows.Forms.Button();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.label_numero = new System.Windows.Forms.Label();
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(443, 477);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(99, 33);
+            this.btn_salir.TabIndex = 13;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // txt_tabla
+            // 
+            this.txt_tabla.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tabla.Location = new System.Drawing.Point(275, 141);
+            this.txt_tabla.Multiline = true;
+            this.txt_tabla.Name = "txt_tabla";
+            this.txt_tabla.Size = new System.Drawing.Size(267, 330);
+            this.txt_tabla.TabIndex = 12;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(275, 477);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(162, 33);
+            this.btn_limpiar.TabIndex = 11;
+            this.btn_limpiar.Text = "Limpiar valores";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_desplegar
+            // 
+            this.btn_desplegar.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_desplegar.Location = new System.Drawing.Point(353, 102);
+            this.btn_desplegar.Name = "btn_desplegar";
+            this.btn_desplegar.Size = new System.Drawing.Size(118, 33);
+            this.btn_desplegar.TabIndex = 10;
+            this.btn_desplegar.Text = "Desplegar";
+            this.btn_desplegar.UseVisualStyleBackColor = true;
+            this.btn_desplegar.Click += new System.EventHandler(this.btn_desplegar_Click);
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_numero.Location = new System.Drawing.Point(442, 67);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(100, 29);
+            this.txt_numero.TabIndex = 9;
+            // 
+            // label_numero
+            // 
+            this.label_numero.AutoSize = true;
+            this.label_numero.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_numero.Location = new System.Drawing.Point(271, 70);
+            this.label_numero.Name = "label_numero";
+            this.label_numero.Size = new System.Drawing.Size(165, 22);
+            this.label_numero.TabIndex = 8;
+            this.label_numero.Text = "Número a ingresar";
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.Location = new System.Drawing.Point(270, 9);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(279, 29);
+            this.label_titulo.TabIndex = 7;
+            this.label_titulo.Text = "Tabla de multiplicación";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.txt_tabla);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_desplegar);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.label_numero);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.TextBox txt_tabla;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_desplegar;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.Label label_numero;
+        private System.Windows.Forms.Label label_titulo;
+    }
+}
+
---
+++ Unidad_4/Tabla_Multiplicacion_Ciclo_Do_While/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabla_Multiplicacion_Ciclo_Do_While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_desplegar_Click(object sender, EventArgs e)
+        {
+            //declaracion de variables
+            double n, c, res;
+
+            //entrada de valor n
+            n = System.Double.Parse(txt_numero.Text);
+
+            //ciclo do while
+            c = 1;
+            do
+            {
+                res = n * c;
+                txt_tabla.Text += n.ToString() + " * " + c.ToString() + " = " + res.ToString() + "\r\n";
+                c++;
+            } while (c <= 10);
+
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            foreach (var txt in Controls)
+            {
+                if (txt is TextBox)
+                    ((TextBox)txt).Clear();
+            }
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
